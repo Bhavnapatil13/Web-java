68b2bc1d7603824ddb38c642f9388b1d514fdc8f
Bhavna Zaware <bhavnazaware42@gmail.com>
2025-04-20 14:14:40 +0530

    ..

---
+++ Assignment-4/Assignment04.java
@@ -0,0 +1,27 @@
+package com.sunbeam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class Assignment04Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment04Application.class, args);
+	}
+	
+	@Qualifier()
+       @Autowired
+      private Values val;
+	
+	@Override
+	public void run(String... args) throws Exception {
+         val.sendValue();
+		
+	}	
+}
